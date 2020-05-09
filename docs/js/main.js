115705a8bb58375236d220dd40cff2544968ab86
gurovoleg <39372887+gurovoleg@users.noreply.github.com>
2020-05-09 18:08:21 +0300

    return saveas correct version

--- docs/js/main.js
+++ docs/js/main.js
@@ -9,10 +9,9 @@ const spinner = document.querySelector("#spinner")
 const inputFile = document.querySelector("#file")
 let board = null
 let sparePieces = false
-const isMSBrowser = navigator.userAgent.includes('MSIE') || navigator.userAgent.includes('Edge')
 
 // const m = navigator.userAgent.match(/(opera|chrome|safari|firefox|edge|msie)\/?\s*(\d+)/i)
-const m = navigator.userAgent.match(/(opera|chrome|safari|firefox|edge|msie)/i) // tests
+
 
 createBoard()
 
@@ -69,38 +68,21 @@ function getCanvas () {
        // document.body.appendChild(canvas)
        canvas.toBlob(function(blob) {
          console.log('blob data: ', blob)
-         // IE & Edge (не передает фигуры на доску)
-         if (isMSBrowser) {
-           navigator.msSaveBlob(blob, inputFile.value || 'image.jpg');   
-           spinner.classList.add('d-none')
-         } else {
-           download(blob) 
-         }
+         saveAs(blob, inputFile.value || 'image.jpg');   
+         spinner.classList.add('d-none')
+         // download(blob) 
        }, 'image/jpeg', 1)
     })
 }
 
-function download (blob) {
-  fileName = inputFile.value || 'image.jpg'
-  // берём Blob и создаём уникальный URL для него в формате blob:<origin>/<uuid>
-  link.href = URL.createObjectURL(blob)
-  link.download = fileName
-  link.click()
-  spinner.classList.add('d-none')
-  // удаляем внутреннюю ссылку на Blob, что позволит браузеру очистить память
-  URL.revokeObjectURL(link.href)
-}
-
-
-// saveButton.addEventListener('click', () => {
-// 	html2canvas(target, { allowTaint: true, useCORS: false })
-// 		.then(canvas => {
-//     	console.log(canvas)
-//     	// document.body.appendChild(canvas)
-//     	// const dataURL = canvas.toDataURL("image/jpeg")
-// 			// console.log(dataURL)
-//     	// canvas.toBlob(function(blob) {
-//     	//     saveAs(blob, "pretty test-image.png");
-//     	// });
-// 		});
-// })
+// download with link
+// function download (blob) {
+//   fileName = inputFile.value || 'image.jpg'
+//   // берём Blob и создаём уникальный URL для него в формате blob:<origin>/<uuid>
+//   link.href = URL.createObjectURL(blob)
+//   link.download = fileName
+//   link.click()
+//   spinner.classList.add('d-none')
+//   // удаляем внутреннюю ссылку на Blob, что позволит браузеру очистить память
+//   URL.revokeObjectURL(link.href)
+// }
