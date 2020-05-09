b8665bab2c1d8980f57da0df03fcb61b979908fe
gurovoleg <39372887+gurovoleg@users.noreply.github.com>
2020-05-09 17:44:00 +0300

    added saveas

--- docs/js/main.js
+++ docs/js/main.js
@@ -9,6 +9,11 @@ const spinner = document.querySelector("#spinner")
 const inputFile = document.querySelector("#file")
 let board = null
 let sparePieces = false
+const isMSBrowser = navigator.userAgent.includes('MSIE') || navigator.userAgent.includes('Edge')
+
+// const m = navigator.userAgent.match(/(opera|chrome|safari|firefox|edge|msie)\/?\s*(\d+)/i)
+const m = navigator.userAgent.match(/(opera|chrome|safari|firefox|edge|msie)/i)
+console.log(m)
 
 createBoard()
 
@@ -18,7 +23,7 @@ function createBoard () {
     draggable: true,
     position: 'start',
     dropOffBoard: 'trash',
-    sparePieces
+    sparePieces: sparePieces
 	}
 
   board = Chessboard('board', config)
@@ -61,11 +66,19 @@ function stopEventListeners () {
 function getCanvas () {
   spinner.classList.remove('d-none')
   html2canvas(target, { scale: 3 })
-    .then(canvas => {
+    .then(function (canvas) {
        // document.body.appendChild(canvas)
        canvas.toBlob(function(blob) {
          console.log('blob data: ', blob)
-         download(blob) 
+         // IE & Edge (не передает фигуры на доску)
+         if (isMSBrowser) {
+           navigator.msSaveBlob(blob, inputFile.value || 'image.jpg');   
+           spinner.classList.add('d-none')
+         } else {
+           saveAs(blob, 'image.jpg')
+           spinner.classList.add('d-none')
+           // download(blob) 
+         }
        }, 'image/jpeg', 1)
     })
 }
